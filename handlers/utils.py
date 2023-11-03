9536d8c33650d7d3701a15f1d168f918fa031b3a
Javier Gracia Carpio <jagracar@gmail.com>
2023-11-03 11:33:30 +0100

    Uses hicdex method

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,5 +1,25 @@
+import json
+from contextlib import suppress
 
-def hex_to_utf8(hex_string):
+def clean_null_bytes(string: str) -> str:
+    if string is None:
+        return ''
+    if type(string) is dict:
+        return json.dumps(string)
+    else:
+        return ''.join(string.split('\x00'))
+
+
+def hex_to_utf8(hexbytes: str) -> str:
+    string = None
+    with suppress(Exception):
+        try:
+            string = bytes.fromhex(hexbytes).decode()
+        except Exception:
+            string = bytes.fromhex(hexbytes).decode('latin-1')
+    return clean_null_bytes(string or '')
+
+def hex_to_utf8_old(hex_string):
     return bytes.fromhex(hex_string).decode("utf-8", errors="replace")
 
 def first_key(dictionary):
